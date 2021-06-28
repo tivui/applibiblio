8463f4f41a7abdb36aa39bbcd26e37153b06d0b8
stag <stag@formation-ldnr-5CG62366N4>
2021-06-28 15:30:34 +0200

    Controller mapping emprunt

---
+++ src/main/java/fr/ldnr/groupe2/mavenprojectbiblio/BiblioController.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.ldnr.groupe2.mavenprojectbiblio;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author stag
+ */
+
+@RestController
+public class BiblioController {
+    
+    public static final Logger logger = LoggerFactory.getLogger(BiblioController.class);
+    
+    
+    //Contrôleur qui relie la page html emprunt.html avec l'url /emprunt
+    @Controller
+	public class HtmlPageController {
+	    @GetMapping("/emprunt")
+	    public String getTestPage() {
+	        return "emprunt.html";
+	    }
+	}
+    
+}
