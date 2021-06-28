a8d279df5b2731d58a61f4c871161fd5da63299c
stag <stag@formation-ldnr-5CG62366N4>
2021-06-28 16:46:03 +0200

    testConfiguration base de données Hibernate et création de la classe Entity Livre

--- src/main/java/fr/ldnr/groupe2/mavenprojectbiblio/BiblioController.java
+++ src/main/java/fr/ldnr/groupe2/mavenprojectbiblio/BiblioController.java
@@ -5,8 +5,10 @@
  */
 package fr.ldnr.groupe2.mavenprojectbiblio;
 
+import org.hibernate.SessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,20 +17,23 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author stag
  */
-
 @RestController
 public class BiblioController {
-    
+
     public static final Logger logger = LoggerFactory.getLogger(BiblioController.class);
-    
-    
+
+    //branchement automatique à la session Hibernate
+    @Autowired
+    public SessionFactory sessionFactory;
+
     //Contrôleur qui relie la page html emprunt.html avec l'url /emprunt
     @Controller
-	public class HtmlPageController {
-	    @GetMapping("/emprunt")
-	    public String getTestPage() {
-	        return "emprunt.html";
-	    }
-	}
-    
+    public class HtmlPageController {
+
+        @GetMapping("/emprunt")
+        public String getTestPage() {
+            return "emprunt.html";
+        }
+    }
+
 }
