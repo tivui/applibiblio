7a5a7b73a1729e39b945fa2b4dec498eaee24c3b
stag <stag@formation-ldnr-5CG62366N4>
2021-06-29 12:19:55 +0200

    Config pour le travail séparé

--- src/main/java/fr/ldnr/groupe2/mavenprojectbiblio/BiblioMain.java
+++ src/main/java/fr/ldnr/groupe2/mavenprojectbiblio/BiblioMain.java
@@ -6,10 +6,16 @@
 package fr.ldnr.groupe2.mavenprojectbiblio;
 
 
+import java.util.GregorianCalendar;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 
 /**
  *
@@ -20,9 +26,13 @@ public class BiblioMain {
     
     public static final Logger logger = LoggerFactory.getLogger(BiblioMain.class);
       
+    
     public static void main(String[] args) {
+ 
         SpringApplication.run(BiblioMain.class, args);
 	logger.info("Lancement de l'application Biblio !!!");
+        
+
     }
     
 }
